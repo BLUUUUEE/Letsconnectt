0f0dbe9f896196c8ac1afe5937432e00895e487d
BLUUUUEE <137717684+BLUUUUEE@users.noreply.github.com>
2024-07-11 17:30:49 +0530

    backend linking through the deployed url

--- FRONTEND/src/components/Auth/Register.jsx
+++ FRONTEND/src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const {data} = await axios.post(
-        "http://localhost:4000/api/v1/user/register", //url
+        "https://letsconnectt.onrender.com/api/v1/user/register", //url
         {name,email,password,phone,role}, //data
         {
           withCredentials: true,
